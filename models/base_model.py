d9a39bd58f51114f785c8184b648122c71ed631c
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2023-10-11 15:46:24 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -18,7 +18,7 @@ class BaseModel:
         self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
-        if kwargs:
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
                     self.__dict__[k] = datetime.strptime(v, tform)
