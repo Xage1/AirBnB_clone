3a50b116f73e338afdc2eb4e68045edb133b7bc8
Kenneth Muketha <89647552+Xage1@users.noreply.github.com>
2023-10-11 15:59:18 +0300

    Update console.py

--- console.py
+++ console.py
@@ -3,7 +3,7 @@
 import cmd
 import re
 from shlex import split
-from models import storage
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -12,6 +12,8 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 
+storage = FileStorage()
+storage.reload()
 
 def parse(arg):
     curly_braces = re.search(r"\{(.*?)\}", arg)
