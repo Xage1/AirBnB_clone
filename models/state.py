a662ca54a52a15a6b622d66bfcef8dca39fb9a42
Xage1 <kennethkiarie555@gmail.com>
2023-10-16 00:17:06 -0700

    Update on models state

--- models/state.py
+++ models/state.py
@@ -2,26 +2,25 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from models.city import City
-from sqlalchemy import Column, String
+from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import getenv
-import models
-
+import os
 
 class State(BaseModel, Base):
     """ State class """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state')
+        cities = relationship('City', backref='state', cascade="all, delete-orphan")
     else:
+        name = ""
+
         @property
         def cities(self):
             """ Returns the list of City instances with state_id equals
             to the current State.id """
-            dict_city = models.storage.all(City)
-            store = []
-            for city in dict_city.values():
+            city_list = []
+            for city in models.storage.all(City).values():
                 if city.state_id == self.id:
-                    store.append(city)
-            return store
+                    city_list.append(city)
+            return city_list
